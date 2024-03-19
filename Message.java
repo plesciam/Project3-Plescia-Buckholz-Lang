1a3dd7358e1ff60c25ba55276679a2e2875413dc
plesciam <plesciam@merrimack.edu>
2024-03-19 12:53:42 -0400

    Message file changed

--- Message.java
+++ Message.java
@@ -3,82 +3,27 @@
  */
  public class Message 
 {
-        private int Message;
-        private int InetAddress;
-        private byte[];
-  
-            {
-             int InetAddress = setInetAddress(0);
+        {
             }
             public void getMessage() 
             {
-                Message = setMessage(0);
+                setMessage(0);
             }
 
             public int setMessage(int message) {
-                this.Message = message;
                 return message;
             }
 
             public void getInetAddress() 
             {
-                InetAddress = setInetAddress(0);
+                setInetAddress(0);
                 
             }
             
             public int setInetAddress(int inetAddress) {
-                this.InetAddress = inetAddress;
                 return inetAddress;
             }
-    }
+
+}
 
         
-      
-    //             int setInetAddress(String inetAddress) 
-    //             int InetAddress;
-    //             int getInetAddress() 
-    //             String inetAddress;
-    //             String InetAddress = inetAddress;  
-                
-               
-                
-    //         }     
-            //
-    //         public Message(String inetAddress, String message) 
-            
-    //         private String Message;
-    //             Message = message; 
-    //         public String getMessage() {}
-            
-    //         return Message;
-    //         }
-    //         public void setMessage(String message) 
-    //         {
-    //         Message = message;
-    //         }
-          
-    //         int byte[];
-    //         }
-        
-        // public Message(int inetAddress, int message) {
-        //     InetAddress = inetAddress;
-        //     Message = message;
-        // }
-//         public int getInetAddress() {
-//             return InetAddress;
-//         }
-//         public void setInetAddress(int inetAddress) {
-//             InetAddress = inetAddress;
-//         }
-//         public int getMessage() {
-//             return Message;
-//         }
-//         public void setMessage(int message) {
-//             Message = message;
-//         }
-//         @Override
-//         public String toString() {
-//             return "Message [InetAddress=" + InetAddress + ", Message=" + Message + "]";
-//         }
-    
- //}
