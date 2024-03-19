375f5d075047a5f7175704aaeeae2a2a4069e06b
plesciam <plesciam@merrimack.edu>
2024-03-19 13:03:45 -0400

    ReliableSocket file changed

--- ReliableSocket.java
+++ ReliableSocket.java
@@ -1,65 +1,20 @@
-import java.util.zip.Checksum;
+import java.io.IOException;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Timer;
 
-/**
- * ReliableSocket
- */
-public class ReliableSocket {
+public class ReliableSocket 
+{
+    private DatagramSocket socket;
+    private HashMap<ConnectionIdentifier, SenderState> senderStates;
+    private HashMap<ConnectionIdentifier, ReceiverState> receiverStates;
+    private HashMap<ConnectionIdentifier, Timer> timers;
 
- public void update(byte[] b, int off, int len) throws ArrayIndexOutOfBoundsException {
-
-    public long getValue(Checksum) {
-
-        return CRC-32 value
-
-        }
-    
-    
-        public update(byte[] b) {
-
-        int b; 
-        int off; 
-        int len;
-        int buffer;
-
-        }
-    
-        
-        
-        public void reset() {
-            
-        }
-
-    
-        
-
-    
-
-        
-
+    // Constructor to connect to a remote machine
+    public ReliableSocket(InetAddress remoteAddress, int remotePort) throws Socket Exception 
+    {
+        socket = new DatagramSocket();
+        socket.connect(remoteAddress, remotePort);
+        senderStates = new Hashmap<>();
     }
 }
-  
-
-        
-
-    
-
-
-
-// import java.io.IOException;
-// import java.net.*;
-// import java.util.HashMap;
-// import java.util.Timer;
-
-// public class ReliableSocket {
-//     private DatagramSocket socket;
-//     private HashMap<ConnectionIdentifier, SenderState> senderStates;
-//     private HashMap<ConnectionIdentifier, ReceiverState> receiverStates;
-//     private HashMap<ConnectionIdentifier, Timer> timers;
-
-//     // Constructor to connect to a remote machine
-//     public ReliableSocket(InetAddress remoteAddress, int remotePort) throws Socket Exception {
-//         socket = new DatagramSocket();
-//         socket.connect(remoteAddress, remotePort);
-//         senderStates = new Hashmap<>();
-//     }
