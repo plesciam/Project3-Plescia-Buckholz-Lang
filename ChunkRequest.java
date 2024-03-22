086a7be9ff29f0d1e7c48ecf9e604d952c6dbc95
ABuckholz <buckholza@merrimack.edu>
2024-03-22 14:54:34 -0400

    Added more methods but cannot figure out errors for toJSON and serialize methods

--- ChunkRequest.java
+++ ChunkRequest.java
@@ -1,7 +1,11 @@
-import merrimackutil.json.JSONSerializable;
+import java.io.InvalidObjectException;
+import java.nio.charset.StandardCharsets;
+import merrimackutil.json.*;
+import merrimackutil.json.JSONSerializable.*;
 import merrimackutil.json.types.JSONObject;
 import merrimackutil.json.types.JSONType;
-
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class ChunkRequest implements JSONSerializable {
     private String fileName;
@@ -20,12 +24,45 @@ public class ChunkRequest implements JSONSerializable {
         return chunkId;
     }
 
+    public JSONType toJSONType(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("file-name", fileName);
+        jsonObject.put("chunk-id", chunkId);
+        return jsonObject;
+    }
+
     @Override
-    public JSONType toJSON(){
+    public String toJSON(){
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("file-name", fileName);
         jsonObject.put("chunk-id", chunkId);
         return jsonObject.toString();
     }
+    @Override
+    public byte[] serialize(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("file-name", fileName);
+        jsonObject.put("chunk-id", chunkId);
+
+        String jsonString = toJSON();
+        return jsonString.getBytes(StandardCharsets.UTF_8);
+
+        try{
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            outputStream.write(jsonString.getBytes(StandardCharsets.UTF_8));
+            return outputStream.toByteArray();
+        } catch (IOException e){
+            // Handle IOException
+            e.printStackTrace(); // or log the exception
+            return null;
+        }
+    }
+
+    @Override
+    public void deserialize(JSONType arg0) throws InvalidObjectException {
+        if(!(arg0 instanceof JSONObject))
+        throw new InvalidObjectException("Invalid JSONType for deserialization");
+    }
+        
 
 }
--- ChunkResponse.java
+++ ChunkResponse.java
@@ -1,14 +1,16 @@
 import java.io.InvalidObjectException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import merrimackutil.json.JSONSerializable;
+import merrimackutil.json.types.JSONObject;
 import merrimackutil.json.types.JSONType;
 
 public class ChunkResponse implements JSONSerializable{
     private String fileName;
     private int chunkId;
-    private byte[] chunkData;
+    private String chunkData;
 
-    public ChunkResponse(String fileName, int chunkId, byte[] chunkData){
+    public ChunkResponse(String fileName, int chunkId, String chunkData){
         this.fileName = fileName;
         this.chunkId = chunkId;
         this.chunkData = chunkData;
@@ -22,30 +24,42 @@ public class ChunkResponse implements JSONSerializable{
         return chunkId;
     }
 
-    public byte[] getChunkData(){
+    public String getChunkData(){
         return chunkData;
+    }
+
+    public JSONType toJSONType(){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("file-name", fileName);
+        jsonObject.put("chunk-id", chunkId);
+        jsonObject.put("chunk-data", chunkData);
+        return jsonObject;
     }
 
     @Override
     public String toJSON(){
-        return "{\"file-name\": \"" + fileName + "\", \"chunk-id\": " + chunkId + ", \"chunk-data\": \"" + Base64.getEncoder().encodeToString(chunkData) + "\"}";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("file-name", fileName);
+        jsonObject.put("chunk-id", chunkId);
+        jsonObject.put("chunk-data", chunkData);
+        return jsonObject.toString();
     }
 
     @Override
     public void deserialize(JSONType arg0) throws InvalidObjectException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deserialize'");
+        if(!(arg0 instanceof JSONObject)){
+            throw new InvalidObjectException("Invalid JSONType: Expected JSONObject");
+        }
+
+        JSONObject jsonObject = (JSONObject) arg0;
+        fileName = jsonObject.getString("file-name");
+        chunkId = jsonObject.getInt("chunk-id");
+        chunkData = jsonObject.getString("chunk-data");
     }
 
     @Override
-    public String serialize() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'serialize'");
-    }
-
-    @Override
-    public JSONType toJSONType() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'toJSONType'");
+    public byte[] serialize() {
+        String jsonString = toJSON();
+        return jsonString.getBytes(StandardCharsets.UTF_8);
     }
 }
